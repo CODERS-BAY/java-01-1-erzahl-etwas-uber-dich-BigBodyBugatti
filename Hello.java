15a41b0bacc366314f6a64d69facd46f1f6cfe74
BigBodyBugatti <84071231+BigBodyBugatti@users.noreply.github.com>
2021-07-22 11:54:58 +0200

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        int alter = 33;
+        String Vorname = "Petar";
+        char geschlecht = 'm';
+        String Nachname = "Novakovic";
+        double Notendurchschnitt = 1.4;
+        boolean verheiratet = false;
+
+
+        System.out.println("Ich bin" + " " + alter + " Jahre alt " +geschlecht + " " +verheiratet);
+
+
+    }
+}
